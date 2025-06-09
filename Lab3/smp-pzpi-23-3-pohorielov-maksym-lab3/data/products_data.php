a81dbcb375490ad3937ab74fe51af19dd233d281
NurePohorielovMaksym <maksym.pohorielov@nure.ua>
2025-06-09 10:45:45 +0300

    Update products_data.php

--- Lab3/smp-pzpi-23-3-pohorielov-maksym-lab3/data/products_data.php
+++ Lab3/smp-pzpi-23-3-pohorielov-maksym-lab3/data/products_data.php
@@ -1,1 +1,22 @@
+<?php
+$products = [
+    1 => [
+        'id' => 1,
+        'name' => 'Fanta',
+        'price' => 1,
+        'description' => 'Orange flavored carbonated drink'
+    ],
+    2 => [
+        'id' => 2,
+        'name' => 'Sprite',
+        'price' => 1,
+        'description' => 'Clear lemon-lime flavored carbonated drink',
+    ],
+    3 => [
+        'id' => 3,
+        'name' => 'Nuts',
+        'price' => 1,
+        'description' => 'Assorted nuts mix',
+    ]
+];
 
