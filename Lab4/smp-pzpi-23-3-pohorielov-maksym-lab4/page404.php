6ae35c07246bbf49b3339d7500d1271dc05df50b
NurePohorielovMaksym <maksym.pohorielov@nure.ua>
2025-06-10 08:23:16 +0300

    Update page404.php

--- Lab4/smp-pzpi-23-3-pohorielov-maksym-lab4/page404.php
+++ Lab4/smp-pzpi-23-3-pohorielov-maksym-lab4/page404.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>Помилка 404 - Сторінка не знайдена</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+    <div class="container mt-5 d-flex justify-content-center align-items-center" style="min-height: 70vh;">
+        <div>
+            <h1 class="text-danger">⛔ Помилка 404</h1>
+            <p class="lead">Сторінка не знайдена. Будь ласка, перевірте URL або <a href="index.php">поверніться на головну</a>.</p>
+        </div>
+    </div>
+</html>
